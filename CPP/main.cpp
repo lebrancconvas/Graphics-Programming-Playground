dc7297ec574b945809b53f5643ab58e32759d444
lebrancconvas <lebrancconvas@gmail.com>
2022-06-13 10:43:28 +0700

    ✨ Create New Image.

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -4,12 +4,12 @@
 int main()
 {
 	// Set Image Size. This is the size of the image that will be created.
-	const int WIDTH = 256;
-	const int HEIGHT = 256;
+	const int WIDTH = 800;
+	const int HEIGHT = 600;
 
 	// Set the output file. 
-	std::ofstream fout("output/ppm/output.ppm");
-	// std::ofstream fout("log/output.txt");
+	std::ofstream fout("output/ppm/output_02.ppm");
+	// std::ofstream fout("log/output_02.txt");
 
 	// Catch Event When the file writting is failed. 
 	if(fout.fail()) return -1; // Return Error. 
@@ -31,4 +31,4 @@ int main()
 	}
 
 	return 0; // Return No Error. 
-}
+} 
