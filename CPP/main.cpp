ea4a6fbd0e836880a4253d7c28781af1ecadc8cd
lebrancconvas <lebrancconvas@gmail.com>
2022-06-13 10:25:18 +0700

    💬 Update Format of PPM and Text.

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -8,8 +8,8 @@ int main()
 	const int HEIGHT = 256;
 
 	// Set the output file. 
-	std::ofstream fout("output/output.ppm");
-	// std::ofstream fout("log/output.txt");
+	// std::ofstream fout("output/output.ppm");
+	std::ofstream fout("log/output.txt");
 
 	// Catch Event When the file writting is failed. 
 	if(fout.fail()) return -1; // Return Error. 
@@ -25,8 +25,9 @@ int main()
 		{
 			fout << rand() % 256 << " "; // Red Value. 
 			fout << rand() % 256 << " "; // Green Value.  
-			fout << rand() % 256 << " \n"; // Blue Value. 
+			fout << rand() % 256 << "  "; // Blue Value. 
 		}
+		fout << "\n"; // New Line.
 	}
 
 	return 0; // Return No Error. 
